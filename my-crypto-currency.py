2ff38dcbd9f49604e0cff78908717238b8c2801f
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2017-12-19 07:28:54 +0530

    started

--- my-crypto-currency.py
+++ my-crypto-currency.py
@@ -1,2 +1,19 @@
+import hashlib
 
+class block:
+    def __init__(self, index, timestamp, data, previousHash):
+        self.index=index
+        self.timestamp=timestamp
+        self.data=data
+        self.previousHash=previousHash
+        self.hash = ''
 
+    def calculate_Hash():
+        return str(hashlib.sha256(self.index + self.timestamp + self.previousHash + str(self.data)))
+
+class blockchain:
+    def __init__(self):
+        self.chain=[]
+
+    def create_genesis_block():
+        return block(0,str(now),"Genesis Block", 0)
